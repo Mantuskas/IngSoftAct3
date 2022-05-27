2816a28cd0ed42dc5dd1fcfbf589bf9a886b19bc
Mantas Jakstas <mantasjob8@gmail.com>
2022-05-27 14:28:51 +0200

    Dejamos preparado el archivo Act3GrupalUITests
    Preparamos el archivo quitando codigo innecesarios creado por xcode

---
+++ Act3Grupal/Act3Grupal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Act3Grupal
+//
+//  Created by Mantas Jakstas on 27/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Act3Grupal/Act3GrupalUITests/Act3GrupalUITests.swift
@@ -0,0 +1,21 @@
+//
+//  Act3GrupalUITests.swift
+//  Act3GrupalUITests
+//
+//  Created by Mantas Jakstas on 27/5/22.
+//
+
+import XCTest
+
+class Act3GrupalUITests: XCTestCase {
+    
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+}
