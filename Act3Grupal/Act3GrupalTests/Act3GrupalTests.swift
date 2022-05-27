6456adef0f945b979a264092d0f7d796dd86aa7b
Miguel soto <[miguel.esparza089@comunidadunir.net]>
2022-05-28 00:36:11 +0200

    Añadimos mas test  y refactorizamos codigo.

--- Act3Grupal/Act3GrupalTests/Act3GrupalTests.swift
+++ Act3Grupal/Act3GrupalTests/Act3GrupalTests.swift
@@ -28,78 +28,134 @@ class Act3GrupalTests: XCTestCase {
     func test_CalcularSuma() {
         
         //GIVEN
-        
+        let expectedResult = 13.0
         
         //WHEN
         let sumarDosNumerosNaturales = sut.suma(num1: 10, num2: 3)
         
         //THEN
-        XCTAssertEqual(sumarDosNumerosNaturales, 13, "Deberia de devolver un 13")
+        XCTAssertEqual(sumarDosNumerosNaturales, expectedResult, "Deberia de devolver un \(expectedResult)")
         
+    }
+    
+    func test_CalcularSuma_Incorrecta() {
+        
+        //GIVEN
+        let expectedValue = 20.0
+    
+        //WHEN
+        let result = sut.suma(num1: 10.0, num2: 1.0)
+        //THEN
+        XCTAssertTrue(result != expectedValue)
+
     }
     
     func test_CalcularResta() {
         
         //Given
-        
+        let expectedResult = 3.0
         
         //WHEN
         let sumarDosNumerosNaturales = sut.restar(num1: 5, num2: 2)
         
        //THEN
-        XCTAssertEqual(sumarDosNumerosNaturales, 3, "Deberia de devolver un 3")
+        XCTAssertEqual(sumarDosNumerosNaturales, expectedResult, "Deberia de devolver un \(expectedResult)")
+
+    }
+    
+    func test_CalcularResta_Incorrecta() {
+        //GIVEN
+        let expectedValue = 30.0
+    
+        //WHEN
+        let result = sut.restar(num1: 10.0, num2: 1.0)
+        //THEN
+        XCTAssertTrue(result != expectedValue, "Deberia de dar algo diferente a 30")
 
     }
     
     func test_CalcularMultiplicar() {
         
         //GIVEN
-        
+        let expectedResult = 25.0
         
         //WHEN
         let sumarDosNumerosNaturales = sut.multiplicar(num1: 5, num2: 5)
         
         //THEN
-        XCTAssertEqual(sumarDosNumerosNaturales, 25, "Deberia de devolver un 25")
+        XCTAssertEqual(sumarDosNumerosNaturales, expectedResult, "Deberia de devolver un \(expectedResult)")
         
+    }
+    
+    func test_CalcularMultiplicacion_Incorrecta() {
+        //GIVEN
+        let expectedValue = 4.0
+    
+        //WHEN
+        let result = sut.multiplicar(num1: 2.0, num2: 5.0)
+        //THEN
+        XCTAssertTrue(result != expectedValue, "Deberia devolver un algo diferente a 4")
+
     }
     
     func test_CalcularDivision() {
         
         //GIVEN
-        
+        let expectedResult = 5.0
         
         //WHEN
         let sumarDosNumerosNaturales = sut.dividir(num1: 15, num2: 3)
         //THEN
-        XCTAssertEqual(sumarDosNumerosNaturales, 5, "Deberia de devolver un 0")
+        XCTAssertEqual(sumarDosNumerosNaturales, expectedResult, "Deberia de devolver un \(expectedResult)")
         
         
+    }
+    
+    func test_CalcularDivision_Incorrecta() {
+        //GIVEN
+        let expectedValue = 3.0
+    
+        //WHEN
+        let result = sut.dividir(num1: 21.0, num2: 6.0)
+        //THEN
+        XCTAssertTrue(result != expectedValue, "Deberia devolver un algo diferente a 3")
+
     }
     
     func test_RaizCuadrada() {
         
         //Given
-        
+        let expectedValue = 8.0
         
         //WHEN
         let sumarDosNumerosNaturales = sut.raizCuadrada(num: 64)
 
         //THEN
-        XCTAssertEqual(sumarDosNumerosNaturales, 8.0, "Deberia de devolver un 8.0")
+        XCTAssertEqual(sumarDosNumerosNaturales, expectedValue, "Deberia de devolver un \(expectedValue)")
         
     }
 
     func test_exponencial () {
         
         //Given
-        
+        let expectedValue = 3125
 
         //WHEN
         let sumarDosNumerosNaturales = sut.exponencial(5, 5)
 
         //THEN
-        XCTAssertEqual(sumarDosNumerosNaturales, 3125, "Deberia de devolver un 3125")
+        XCTAssertEqual(sumarDosNumerosNaturales, expectedValue, "Deberia de devolver un \(expectedValue)")
+    }
+    
+    func test_CalcularExponencial_Incorrecta() {
+        //GIVEN
+        let expectedValue = 3125
+    
+        //WHEN
+        let result = sut.exponencial(5, 4)
+        //THEN
+        XCTAssertTrue(result != expectedValue, "Deberia devolver un algo diferente a 3125")
+
     }
 
 }
